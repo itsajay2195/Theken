1808190fdbe2c094d70b0631cb3ddd881867c696
itsajay2195 <ajaykumar.95@yahoo.com>
2021-12-04 16:03:56 +0530

    implementing the header

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { View, Image, TouchableOpacity, Text } from 'react-native'
+
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+
+
+export default function HeaderTabs() {
+
+    return (
+        <View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', margin: 20 }}>
+                <View style={{ flexDirection: 'row' }}>
+                    <Image source={{ uri: 'https://media.istockphoto.com/photos/man-portrait-in-black-and-white-picture-id1135480451?k=20&m=1135480451&s=612x612&w=0&h=I5hnIg8uOr2DWixwQo-q0R5Nvp5Fuwru6IiV0GX-5BY=' }} style={{ width: 40, height: 40, borderRadius: 5 }}></Image>
+                    <View style={{ marginLeft: 10 }}>
+                        <Text style={{ color: 'gray' }}>Welcome</Text>
+                        <Text style={{ fontWeight: '600', fontSize: 16 }}>Ajaykumar Rajasekaran</Text>
+                    </View>
+                </View>
+
+                <Text>Hi</Text>
+            </View>
+
+            <View style={{ marginHorizontal:20}}>
+                <Text style={{fontSize:25,fontWeight:'700'}}>Let the Music </Text>
+                <Text style={{fontSize:25,fontWeight:'700'}}>Take you away </Text>
+            </View>
+        </View>
+
+    )
+}
+
--- src/screens/Album.js
+++ src/screens/Album.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Platform,FlatList } from 'react-native'
+import Header from '../components/Header'
 
 export default function Album() {
     return (
-        <SafeAreaView>
-            <Text>Album screen</Text>
+        // flex:0 is used to make the screen full and hide the bootom of the safeareaview
+        // or we can skipp mentioning the flex property itself
+        <SafeAreaView style={{ borderRadius: 20, backgroundColor: 'white', flex: 0, paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0 }}>
+            {/* Platform.OS === "android" ? StatusBar.currentHeight : 0 = this piece of code is just to ensure that the header is not misplaced on devices with knotch */}
+            <View style={{ marginHorizontal: 10, backgroundColor: 'white', height: 150 }}>
+                <Header></Header>
+            </View>
+            <View style={{ height: '100%', backgroundColor: '#F0F3F4' }}>
+                <View style={{ margin: 30 }}>
+                    <Text style={{ fontSize: 25, fontWeight: '700' }}>Albums </Text>
+                    <FlatList></FlatList>
+                </View>
+
+            </View>
+
         </SafeAreaView>
     )
 }
+//backgroundColor: 'pink',
